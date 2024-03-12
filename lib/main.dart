d94e585d59e13ec1df193074de5d938db3d38fdb
Danilo <danilo.macedo@etec.sp.gov.br>
2024-03-12 20:59:00 -0300

    branch com o código da aula do dia Tue, Mar 12, 2024  8:59:00 PM

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const Aplicacao());
+
+class Aplicacao extends StatelessWidget {
+  const Aplicacao({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Home",
+          style: theme.textTheme.titleLarge?.copyWith(color: Colors.white),
+        ),
+        backgroundColor: Colors.black,
+      ),
+      body: Column(
+        children: [
+          Image.asset(
+            "lib/assets/images/dragonball.jpe",
+            height: 350,
+            fit: BoxFit.fitHeight,
+          ),
+        ],
+      ),
+    );
+  }
+}
